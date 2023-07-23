891d6e9663298d385993d0adfbc20478977084ce
hasna <manelhasnahaddoud@gmail.com>
2023-07-23 17:40:46 +0100

    done

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+function receivesAFunction(callback) {
+    
+    callback();
+  }
+  function returnsANamedFunction() {
+    function namedFunction() {
+    
+
+
+
+    }
+    return namedFunction;
+    }
+
+    
+  function returnsAnAnonymousFunction(){
+return function(){
+
+
+
+
+
+
+
+
+}
+
+  }
